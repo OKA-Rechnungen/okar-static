d9d2ce2e80667f7d6c7a9f1d34071c4c99a207a2
fsanzl <>
2025-11-11 14:57:12 +0100

    updateimprint

--- build_app/scripts/dl_imprint.sh
+++ build_app/scripts/dl_imprint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-REDMINE_ID="24844?format=xhtml&locale="
+REDMINE_ID="24844"
 IMPRINT_XML=./data/imprint.xml
 mkdir -p data
-rm -f ${IMPRINT_XML}
-echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo '<?xml version="1.0" encoding="UTF-8"?>' > ${IMPRINT_XML}
 echo "<root>" >> ${IMPRINT_XML}
 echo '<div lang="de">' >> ${IMPRINT_XML}
-curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}de >> ${IMPRINT_XML}
+curl "https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}?format=xhtml&locale=de" >> ${IMPRINT_XML}
 echo "</div>"  >> ${IMPRINT_XML}
 echo '<div lang="en">' >> ${IMPRINT_XML}
 curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}en >> ${IMPRINT_XML}
